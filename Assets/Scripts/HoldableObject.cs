4113222ae0a767155b9a4e317f0b795aad6db02e
MSI\2220069 <kelvinleungyl@gmail.com>
2023-02-17 12:52:23 +0900

    Update Test Scene: Install Input System package,  Handle Gamepad Input, Placeable Object Rotation, Camera Collider (Wall / Ground Clipping)

--- Assets/Scripts/HoldableObject.cs
+++ Assets/Scripts/HoldableObject.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class HoldableObject : InteractiveObject
 {
+    private PlaceablePoint placedAt;
+
     protected override void Start()
     {
+        placedAt = null;
         gameObject.layer = LayerMask.NameToLayer("Holdable");
         base.Start();
     }
@@ -18,7 +21,23 @@ public class HoldableObject : InteractiveObject
 
     public override void click()
     {
+        if (player.GetComponent<MaidController>().getHolding() != null) return;
         player.GetComponent<MaidController>().hold(gameObject);
         GetComponent<Collider>().enabled = false;
+
+        if (placedAt != null)
+        {
+            GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+            placedAt.releaseObject();
+            placedAt = null;
+        }
+    }
+
+    public void place(PlaceablePoint point)
+    {
+        placedAt = point;
+
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+        point.setObject(gameObject);
     }
 }
