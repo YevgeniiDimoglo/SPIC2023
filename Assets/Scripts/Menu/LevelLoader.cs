4c988e45ef0b600b5995e62d27a391bd96ed172a
Dimoglo Yevgenii <222175@ecc.ac.jp>
2023-02-19 11:39:36 +0900

    Added new model
    Added prefabs and placement mechanic to house level

--- Assets/Scripts/Menu/LevelLoader.cs
+++ Assets/Scripts/Menu/LevelLoader.cs
@@ -14,6 +14,11 @@ public class LevelLoader : MonoBehaviour
         StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
     }
 
+    public void LoadFirstLevel()
+    {
+        StartCoroutine(LoadLevel(0));
+    }
+
     IEnumerator LoadLevel(int levelIndex)
     {
         transition.SetTrigger("Start");
